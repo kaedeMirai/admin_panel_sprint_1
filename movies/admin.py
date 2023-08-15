79b713c5f46011003d7772242bccf0a3772ac782
Mario8602 <hellp8901@gmail.com>
2023-08-15 14:47:21 +0500

    добавлена модель Filmwork, модель зарегистрирована в админке

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Genre
+from .models import Genre, Filmwork
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'created']
+    list_filter = ['created', ]
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'creation_date', 'rating', 'type']
--- movies/models.py
+++ movies/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Genre(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField('name', max_length=255)
-    description = models.TextField('description', blank=True)
+    name = models.CharField(max_length=255, verbose_name='Наименование')
+    description = models.TextField(blank=True, verbose_name='Описание')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -13,3 +13,31 @@ class Genre(models.Model):
         db_table = "content\".\"genre"
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Filmwork(models.Model):
+
+    CHOICE_TYPE = (
+        ('кино', 'movie'),
+        ('тв шоу', 'tv_show')
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    creation_date = models.DateField(verbose_name='Премьера')
+    rating = models.FloatField(verbose_name='Рейтинг')
+    type = models.CharField(max_length=100, choices=CHOICE_TYPE, default='movie', verbose_name='Жанр')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "content\".\"film_work"
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+    def __str__(self) -> str:
+        return self.title
